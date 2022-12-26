70bcc7ff883f6f70ccb896daa9ed821920e1121d
anan826 <david70825@gmail.com>
2022-12-26 23:14:23 +0800

    index and demo done

---
+++ demo/final_58.md
@@ -0,0 +1,27 @@
+### Github repo url
+
+[My github repo](https://github.com/anan826/1111-sweb-1N-demo-211410658.git)
+
+## week 11 git logs
+
+![](./w11.png)
+
+## week 12 git logs
+
+![](./w12.png)
+
+## week 13 git logs
+
+![](./w13.png)
+
+## week 14 git logs
+
+![](./w14.png)
+
+## week 15 git logs
+
+![](./w15.png)
+
+## week 16 git logs
+
+![](./w16.png)
---
+++ js/demo_script_58.js
@@ -0,0 +1,51 @@
+function change(index) {
+
+    const p = document.querySelector('.show-classdemo')
+    console.log('p',p);
+    switch(index) {
+        case 1:
+            p.innerHTML = `<iframe src="./demo/w01_58/index.html" width="100%" height="100%" />`
+            break
+        case 2:
+            p.innerHTML = `<iframe src="./demo/w02_58/card_58.html" width="100%" height="100%" />`
+            break
+        case 3:
+            p.innerHTML = `<iframe src="./demo/w03_58/blog_58.html" width="100%" height="100%" />`
+            break
+        case 4:
+            p.innerHTML = `<iframe src="./demo/w04_58/blog_58.html" width="100%" height="100%" />`
+            break
+        case 5:
+            p.innerHTML = `<iframe src="./demo/w05_58/blog_58.html" width="100%" height="100%" />`
+            break
+        case 6:
+            p.innerHTML = `<iframe src="./demo/w06_58/blog_58.html" width="100%" height="100%" />`
+            break
+        case 7:
+            p.innerHTML = `<iframe src="./demo/w08_58/landing_58.html" width="100%" height="100%" />`
+            break
+        case 8:
+            p.innerHTML = `<iframe src="./demo/w09_58/landing_58.html" width="100%" height="100%" />`
+            break
+        case 9:
+            p.innerHTML = `<iframe src="./demo/w11_58_midterm/p4_58/p4_58.html" width="100%" height="100%" />`
+            break
+        case 10:
+            p.innerHTML = `<iframe src="./demo/w12_58/portfolio/index.html" width="100%" height="100%" />`
+            break
+        case 11:
+            p.innerHTML = `<iframe src="./demo/w13_58/portfolio/index.html" width="100%" height="100%" />`
+            break
+        case 12:
+            p.innerHTML = `<iframe src="./demo/w14_58/portfolio/index.html" width="100%" height="100%" />`
+            break
+        case 13:
+            p.innerHTML = `<iframe src="./demo/w15_58/portfolio/index.html" width="100%" height="100%" />`
+            break
+        case 14:
+            p.innerHTML = `<iframe src="./demo/w16_58/portfolio/index.html" width="100%" height="100%" />`
+            break
+        
+        
+    }
+}
